b16b37a5ce8fc932e211cec237928b95df058681
stumpfjo <59803156+stumpfjo@users.noreply.github.com>
2021-01-28 02:08:05 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, json
+
+app = Flask(__name__)
+
+@app.route('/')
+def index(): 
+    return render_template("index.html")
+
+@app.route('/books')
+def books():
+    return render_template("books.html")
+
+@app.route('/borrowers')
+def borrowers():
+    return render_template("borrowers.html")
+
+@app.route('/subjects')
+def subjects():
+    return render_template("subjects.html")
+
+@app.route('/titles')
+def titles():
+    return render_template("titles.html")
+
+@app.route('/items')
+def items():
+    return render_template("items.html")
+    
+@app.route('/checkout')
+def checkout():
+    return render_template("checkout.html")
+
+
