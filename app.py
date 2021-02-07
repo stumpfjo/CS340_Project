35104e4f1d797496e7e3f8f3b0a47c9588696a80
froschj <59715473+froschj@users.noreply.github.com>
2021-02-06 21:58:14 -0600

    got main page updated

--- app.py
+++ app.py
@@ -8,22 +8,20 @@ app = Flask(__name__)
 
 # Routes
 
+'''
 @app.route('/index.html')
 def static_index():
     return render_template("index.html")
+'''
 
 @app.route('/')
 def index():
     return render_template("main.html")
-
+'''
 @app.route('/books')
 def books():
     return render_template("books.html")
-
-@app.route('/borrowers')
-def borrowers():
-    return render_template("borrowers.html")
-
+'''
 @app.route('/borrowers/add_borrowers')
 def add_borrowers():
     return render_template("borrowers/add_borrowers.html")
@@ -59,11 +57,11 @@ def add_subjects():
 @app.route('/subjects/view_subjects')
 def view_subjects():
     return render_template("subjects/view_subjects.html")
-
+'''
 @app.route('/titles')
 def titles():
     return render_template("titles.html")
-
+'''
 @app.route('/titles/add_titles')
 def add_titles():
     return render_template("titles/add_titles.html")
@@ -72,10 +70,14 @@ def add_titles():
 def search_titles():
     return render_template("titles/search_titles.html")
 
-@app.route('/titles/update_title')
+@app.route('/titles/update_title', methods=['GET', 'POST'])
 def update_title():
     return render_template("titles/update_title.html")
 
+@app.route('/items/add_item')
+def add_item():
+    return render_template("/items/add_item.html")
+'''
 @app.route('/items')
 def items():
     return render_template("items.html")
@@ -83,15 +85,15 @@ def items():
 @app.route('/items/checkout')
 def checkout_item():
     return render_template("items/checkout.html")
-
+'''
 @app.route('/items/return_item.html')
 def return_item():
     return render_template("items/return_item.html")
-
+'''
 @app.route('/items/manage')
 def manage_item():
     return render_template("items/collection.html")
-
+'''
 @app.route('/creators')
 def creators():
     return render_template("creators.html")
@@ -103,7 +105,7 @@ def add_creators():
 @app.route('/creators/view_creators')
 def view_creators():
     return render_template("creators/view_creators.html")
-
+'''
 @app.route('/relationships')
 def relationships():
     return render_template("relationships.html")
@@ -124,7 +126,7 @@ def view_title_creators():
 @app.route('/relationships/view_title_subjects')
 def view_title_subjects():
     return render_template("relationships/view_title_subjects.html")
-
+'''
 # Listener
 
 if __name__ == "__main__":
