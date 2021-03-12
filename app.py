84204cc113e1184b1a2865e9c4cf3056c91c88a2
froschj <59715473+froschj@users.noreply.github.com>
2021-03-12 15:20:02 -0600

    extracted state dictionary into data file

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from flask import Flask, render_template, request, json, jsonify, abort, make_re
 import database.db_connector as db
 import os
 from datetime import datetime, timedelta, date
+from data import states
 
 # db_connection = db.connect_to_database()
 
@@ -26,26 +27,6 @@ def get_db():
     return app.db.connection()
 
 # Routes
-
-# Used to populate select box in template
-states = {
-    "AL": "Alabama", "AK": "Alaska", "AZ": "Arizona", "AR": "Arkansas",
-    "CA": "California", "CO": "Colorado", "CT": "Connecticut",
-    "DE": "Delaware", "DC": "District Of Columbia", "FL": "Florida",
-    "GA": "Georgia", "HI": "Hawaii", "ID": "Idaho", "IL": "Illinois",
-    "IN": "Indiana", "IA": "Iowa", "KS": "Kansas", "KY": "Kentucky",
-    "LA": "Louisiana", "ME": "Maine", "MD": "Maryland",
-    "MA": "Massachusetts", "MI": "Michigan", "MN": "Minnesota",
-    "MS": "Mississippi", "MO": "Missouri", "MT": "Montana",
-    "NE": "Nebraska", "NV": "Nevada", "NH": "New Hampshire",
-    "NJ": "New Jersey", "NM": "New Mexico", "NY": "New York",
-    "NC": "North Carolina", "ND": "North Dakota", "OH": "Ohio",
-    "OK": "Oklahoma", "OR": "Oregon", "PA": "Pennsylvania",
-    "RI": "Rhode Island", "SC": "South Carolina", "SD": "South Dakota",
-    "TN": "Tennessee", "TX": "Texas", "UT": "Utah", "VT": "Vermont",
-    "VA": "Virginia", "WA": "Washington", "WV": "West Virginia",
-    "WI": "Wisconsin", "WY": "Wyoming"
-}
 
 @app.route('/')
 def index():
